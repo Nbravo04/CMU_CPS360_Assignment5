7959658c925e27ef073c50f2e06a49a122056055
Nbravo04 <43017063+Nbravo04@users.noreply.github.com>
2019-02-25 11:07:21 -0500

    Update a5.c

--- a5.c
+++ a5.c
@@ -2,7 +2,7 @@
 int main() 
 { 
   void halfaddr(int a, int b, int *sum, int *outcary);
-  void lacadd4(int a, int b, int incary, int *sum, int *outcary);
+  void rcadd4(int a, int b, int incary, int *sum, int *outcary);
 } 
 
 void halfaddr(int a, int b, int *sum, int *outcary){
@@ -11,11 +11,12 @@ void halfaddr(int a, int b, int *sum, int *outcary){
 }
  { simulates HA on bit 0 of a and b, returns sum bit and carry bit }
  
- void lacadd4(int a, int b, int incary, int *sum, int *outcary){
+ void rcadd4(int a, int b, int incary, int *sum, int *outcary){
   fulladdr(a&1,b&1,incary,*sum,*outcary)
-  fulladdr((a>>1)&1,(b>>1)&1,incary,*sum,*outcary)
-  fulladdr((a>>2)&1,(b>>2)&1,incary,*sum,*outcary)
-  fulladdr((a>>3)&1,(b>>3)&1,incary,*sum,*outcary)
+  fulladdr((a>>1)&1,(b>>1)&1,*outcarry,*sum,*outcary)
+  fulladdr((a>>2)&1,(b>>2)&1,*outcarry,*sum,*outcary)
+  fulladdr((a>>3)&1,(b>>3)&1,*outcarry,*sum,*outcary)
  }
- { uses look-ahead-carry scheme and 4 FAs to add lower order 4-bits of
+ { uses ripple-carry scheme and 4 FAs to add lower order 4-bits of
  a and b and incarry is in c }
+
