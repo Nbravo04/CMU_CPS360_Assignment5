0a84ee16fef61cd0534753abc76620b32d932a41
Nbravo04 <43017063+Nbravo04@users.noreply.github.com>
2019-02-26 17:40:20 -0500

    Update a5.c

--- a5.c
+++ a5.c
@@ -5,18 +5,19 @@ int main()
   void rcadd4(int a, int b, int incary, int *sum, int *outcary);
 } 
 
+/*Simulates a half adder using bitwise operators on our inputs a and b that results in our
+  outcarry and sum variables.*/
 void halfaddr(int a, int b, int *sum, int *outcary){
   *outcarry = a & b;
   *sum = a ^ b;
 }
- { simulates HA on bit 0 of a and b, returns sum bit and carry bit }
  
+/*Simulates a 4-bit ripple carry adder by using four full adders and using the last outcarry
+  from the previous full adder as the incarry for the nxt full adder.*/
  void rcadd4(int a, int b, int incary, int *sum, int *outcary){
   fulladdr(a&1,b&1,incary,*sum,*outcary)
   fulladdr((a>>1)&1,(b>>1)&1,*outcarry,*sum,*outcary)
   fulladdr((a>>2)&1,(b>>2)&1,*outcarry,*sum,*outcary)
   fulladdr((a>>3)&1,(b>>3)&1,*outcarry,*sum,*outcary)
  }
- { uses ripple-carry scheme and 4 FAs to add lower order 4-bits of
- a and b and incarry is in c }
 
